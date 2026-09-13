3085bfee9050b38dd1c47132a27ba0539044708f
abhishek kumar hansda <118197040+abhishekkrhansda@users.noreply.github.com>
2026-09-13 16:11:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let btn = document.querySelector(".btn");
+let para = document.querySelector(".para");
+
+btn.addEventListener("click",() =>{
+	para.innerHTML = "<h1> Entered Metaverse </h1>";
+})
